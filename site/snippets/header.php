377867127eb3239fe11296e486ff0bd725078618
Pearl Zeng <zhuzeng@umich.edu>
2016-04-04 18:07:55 -0400

    Changes the logo and primary accent color to be more on brand.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -16,7 +16,7 @@
 
   <header class="header cf" role="banner">
     <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
+      <img src="<?php echo url('assets/images/logo.png') ?>" alt="<?php echo $site->title()->html() ?>" />
     </a>
     <?php snippet('menu') ?>
   </header>
