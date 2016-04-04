df004a621b1c06b7ac46cfc51be057e2fa6e434c
Pearl Zeng <zhuzeng@umich.edu>
2016-04-04 18:16:17 -0400

    Panel user setup

---
+++ site/accounts/msc.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: msc
+email: msc-contact@umich.edu
+password: >
+  $2a$10$K6iR4gTlCfyzsUDYUdWN4uAfmcysP9i1GHOtDC8MoxUhJRNGHnwQ6
+language: en
+role: admin
+history:
+  - about
