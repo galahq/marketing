379fd83dbe149632e78474088b8334f1e7fe1e16
Pearl Zeng <zhuzeng@umich.edu>
2016-05-10 11:05:14 -0400

    Account

---
+++ site/accounts/mscpearl.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: mscpearl
+email: zhuzeng@umich.edu
+password: >
+  $2a$10$6kKD82kWDMe1iw66XYqoee0try3/wlE/Yb80qJDQjNspQiuYJtK.O
+language: en
+role: admin
