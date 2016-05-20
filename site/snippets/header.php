6bcbdbab3fe78f6aa6ae1a988bd4d87db0e9c99a
Pearl Zeng <zhuzeng@umich.edu>
2016-05-20 13:14:58 -0400

    Changes menu, font; update kirby to 2.3.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
-
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
   <?php echo css('assets/css/main.css') ?>
+  <script type="text/javascript">
+      (function() {
+          var path = '//easy.myfonts.net/v2/js?sid=125718(font-family=Freight+Text+Book+SC)&sid=125720(font-family=Freight+Text+Book+Italic)&sid=125721(font-family=Freight+Text+Book)&sid=125770(font-family=Freight+Text+Medium+Italic)&key=ZIMvxUr7nR',
+              protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+              trial = document.createElement('script');
+          trial.type = 'text/javascript';
+          trial.async = true;
+          trial.src = protocol + path;
+          var head = document.getElementsByTagName("head")[0];
+          head.appendChild(trial);
+      })();
+  </script>
 
 </head>
 <body>
