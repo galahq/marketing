36400f4ed78ae80230513f94cdf3b39c793e65dc
Cameron Bothner <cameronbothner@gmail.com>
2016-06-22 13:38:18 -0400

    Cameron's account

---
+++ site/accounts/cameron.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: cameron
+firstname: Cameron
+lastname: Bothner
+email: cameronbothner@umich.edu
+password: >
+  $2a$10$nMRngq9O1nuacOtiBrTwMujFlydmV7L7kG2tyqDSxdNzHJIF5/A9a
+language: en
+role: admin
+history:
+  - home
