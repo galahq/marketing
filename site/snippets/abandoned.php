7075074e38cc947a559e157be860db5681e26714
Pearl Zeng <zhuzeng@umich.edu>
2016-07-09 17:25:16 -0400

    Deletion of codes.

--- site/snippets/abandoned.php
+++ site/snippets/abandoned.php
@@ -35,3 +35,54 @@
     </section>
   <?php endforeach ?>
 </section>
+
+
+
+<div class="menubar uppercase">
+  <div class="site">
+    <?php if($page->parent()->visible()): ?>
+    <p><?php echo html($page->parent()->title()) ?></p>
+    <?php else: ?>
+    <p><?php echo html($page->title()) ?></p>
+    <?php endif ?>
+  </div>
+</div>
+
+
+//footer menu original
+<ul class="footermenu">
+  <?php foreach($items as $item): ?>
+  <li>
+    <a<?php e($item->isOpen(), ' class="active"') ?> href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a>
+
+    <?php
+    // get all children for the current menu item
+    $children = $item->children()->visible();
+
+    // display the submenu if children are available
+    if($children->count() > 0):
+    ?>
+    <ul>
+      <?php foreach($children as $child): ?>
+      <li><a<?php e($child->isOpen(), ' class="active"') ?> href="<?php echo $child->url() ?>"><?php echo $child->title()->html() ?></a></li>
+
+      <?php
+      // get all children for the current menu item
+      $grandchildren = $child->children()->visible();
+
+      // display the submenu if children are available
+      if($grandchildren->count() > 0):
+      ?>
+      <ul>
+        <?php foreach($grandchildren as $grandchild): ?>
+        <li><a<?php e($grandchild->isOpen(), ' class="active"') ?> href="<?php echo $grandchild->url() ?>"><?php echo $grandchild->title()->html() ?></a></li>
+        <?php endforeach ?>
+      </ul>
+      <?php endif ?>
+      <?php endforeach ?>
+    </ul>
+    <?php endif ?>
+  </li>
+  <?php endforeach ?>
+</ul>
+<?php endif ?>
