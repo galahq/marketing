a6ecdb2de667c902d328c32b35cb5d4378d086fe
Pearl Zeng <zhuzeng@umich.edu>
2016-07-10 11:09:33 -0400

    Adds a separate template for elements children.

---
+++ site/templates/elementchild.php
@@ -0,0 +1,18 @@
+<?php snippet('head') ?>
+<?php snippet('header') ?>
+
+<main class="main" role="main">
+
+  <div>
+
+    <article class="site cf">
+
+      <?php echo str_replace('(\\', '(', kirbytext($page->text())) ?>
+
+    </article>
+
+  </div>
+
+</main>
+
+<?php snippet('footer') ?>
