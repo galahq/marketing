bac2dd2c6cd07f04389af7abfdab28bd7769725f
Pearl Zeng <zhuzeng@umich.edu>
2016-07-10 16:31:01 -0400

    Adds header bg.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,11 +2,13 @@
   <?php if($page->isHomePage()): ?>
     <?php $bannerImage = 'content/home/topbanner.jpg';?>
     <section class="intro" style='background: -webkit-linear-gradient(top, rgba(0,0,0,0) 40%, rgba(73,48,146,0.8) 100%), url("<?php echo $bannerImage ?>") 25%; background-size: cover;' >
-      <div class="header cf site">
+      <div class="header cf headerbg">
+        <div class="site">
         <a class="logo" href="<?php echo url() ?>">
           <img src="<?php echo url('assets/images/logo.png') ?>" alt="<?php echo $site->title()->html() ?>" />
         </a>
         <?php snippet('menu') ?>
+      </div>
       </div>
       <div class="site">
         <h1><?php echo $page->tagline() ?></h1>
