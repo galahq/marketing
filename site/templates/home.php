46aac81c32bf280bf16bc5f29229fad81ce007ca
Pearl Zeng <zhuzeng@umich.edu>
2016-07-10 11:10:39 -0400

    Separates contents from structure.

--- site/templates/home.php
+++ site/templates/home.php
@@ -22,7 +22,7 @@
   </div>
   <div class="site">
   <div class="center-text">
-  <button class="button" onclick="window.location.href='<?php echo $pages->find('action')->url() ?>'"><h5>Learn More</h5></button>
+  <button class="button" onclick="window.location.href='action'"><h6>Learn more about actions you can take with MSC</h6></button>
   </div>
   </div>
 </div>
@@ -34,12 +34,16 @@
       <div class="flex-2-8">
         <div class="ver-flex">
           <div class="flex-1-2">
-            <h5 class="uppercase">Narrative</h5>
-            <p>Each MSC tells a story that presents real-world based problem with a challenging decision. Key decision makers, stakeholders, and challenges are being presented. </p>
+            <?php $thumb = $pages->find('element')->find('narrative')->files()->filterBy('filename', '*=', '-thumb')->first()->uri();?>
+            <img class="img-inline" src="<?php echo $thumb ?>" alt="icon of <?php echo $pages->find('element')->find('narrative')->title() ?>" />
+            <h5 class="uppercase cf"><?php echo $pages->find('element')->find('narrative')->title() ?></h5>
+            <p><?php echo $pages->find('element')->find('narrative')->description() ?></p>
           </div>
           <div class="flex-1-2">
-            <h5 class="uppercase">Podcast</h5>
-            <p>MSC podcasts are conversations that expose students to a different side of the issue . Podcasts are interviews with the case decision maker(s) and/or relevant experts.</p>
+            <?php $thumb = $pages->find('element')->find('podcast')->files()->filterBy('filename', '*=', '-thumb')->first()->uri();?>
+            <img class="img-inline" src="<?php echo $thumb ?>" alt="icon of <?php echo $pages->find('element')->find('podcast')->title() ?>" />
+            <h5 class="uppercase cf"><?php echo $pages->find('element')->find('podcast')->title() ?></h5>
+            <p><?php echo $pages->find('element')->find('podcast')->description() ?></p>
           </div>
         </div>
       </div>
@@ -48,27 +52,36 @@
       </figure>
       <div class="flex-2-8">
         <div class="ver-flex">
-          <div class="flex-1-2">
-            <h5 class="uppercase">Multimedia Edgenotes</h5>
-            <p>Edgenotes are jumping-off points for deeper exploration. They are curated from audiovisual sources providing diverse and in-depth insights.</p>
+          <div class="flex-1-2 right">
+            <?php $thumb = $pages->find('element')->find('edgenotes')->files()->filterBy('filename', '*=', '-thumb')->first()->uri();?>
+            <img class="img-inline" src="<?php echo $thumb ?>" alt="icon of <?php echo $pages->find('element')->find('edgenotes')->title() ?>" />
+            <h5 class="uppercase cf"><?php echo $pages->find('element')->find('edgenotes')->title() ?></h5>
+            <p><?php echo $pages->find('element')->find('edgenotes')->description() ?></p>
           </div>
-          <div class="flex-1-2">
-            <h5 class="uppercase">Interactive Learning Exercises</h5>
-            <p>Provide a toolkit of quantitative, technical, analytical and creative competencies; foster an environment for students to practice collaboration, self agency and intercultural communication.</p>
+          <div class="flex-1-2 right">
+            <?php $thumb = $pages->find('element')->find('exercise')->files()->filterBy('filename', '*=', '-thumb')->first()->uri();?>
+            <img class="img-inline" src="<?php echo $thumb ?>" alt="icon of <?php echo $pages->find('element')->find('exercise')->title() ?>" />
+            <h5 class="uppercase cf"><?php echo $pages->find('element')->find('exercise')->title() ?></h5>
+            <p><?php echo $pages->find('element')->find('exercise')->description() ?></p>
           </div>
         </div>
       </div>
     </div>
 
   </div>
+  <div class="site">
+  <div class="center-text">
+  <button class="button" onclick="window.location.href='element'"><h6>Learn more about MSC elements</h6></button>
+  </div>
+  </div>
 
 </div>
 
-<div class="vision">
+<div class="vision-container">
   <div class="site">
     <h3 class="heading"><?php echo str_replace('(\\', '(', kirbytext($page->visionheading())) ?></h3>
   </div>
-</div>
+
 
   <div class="vision site flex-container">
     <?php foreach($page->numbers()->toStructure() as $number): ?>
@@ -77,13 +90,14 @@
         <h4 class="number-description"><?php echo $number->description() ?></h4>
       </div>
     <?php endforeach ?>
-    <div class="conclude">
-      <p>
-        Our goal is to transform sustainability learning by transforming teaching methods in all academic fields. Case-based curricular innovation will lead to a more engaged, participatory learning. It will also enable students to achieve sustainability competencies, which will help develop students towards their full potential—becoming future sustainability leaders.
-      </p>
-    </div>
-    <button class="button" onclick="window.location.href='vision'"><h5>More</h5></button>
   </div>
+  <div class="site">
+  <div class="center-text">
+  <button class="button" onclick="window.location.href='vision'"><h6>Learn more about our vision for MSC</h6></button>
+  </div>
+  </div>
+
+</div>
 
 
   </main>
