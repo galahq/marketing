9ca6674f8ec6927f48d0d3aba9b1c5c31bb3ac4a
Pearl Zeng <zhuzeng@umich.edu>
2016-08-05 12:56:24 -0400

    try different layout for list.

--- site/templates/actionparent.php
+++ site/templates/actionparent.php
@@ -7,19 +7,35 @@
     <div class="site">
       <?php echo str_replace('(\\', '(', kirbytext($page->text())) ?>
     </div>
+    <div class="site">
+      <?php foreach($page->reasons()->toStructure() as $reason): ?>
+        <div class="reason">
+          <span class="reason-value"><?php echo $reason->number() ?></span>
+          <p class="reason-description"><?php echo $reason->description() ?></p>
+        </div>
+      <?php endforeach ?>
+    </div>
   </section>
 
+
+  <ol class="custom-counter">
+  <li>list one</li>
+  <li>list two</li>
+  </ol>
+
+
+
   <div class="actions">
-    <div class="action-items site">
+    <div class="action-items-vertical site">
       <?php foreach($pages->find('action')->children() as $action): ?>
         <div><a href="<?php echo $action->link() ?>">
           <?php if($image = $action->image()): ?>
             <img src="<?php echo $image->url() ?>" alt="Icon for <?php echo html($action->title()) ?>">
           <?php endif ?>
-          <div class="actiontext">
+          <div class="actiontext-vertical">
           <h5 class="uppercase"><?php echo html($action->title()) ?></h5>
           <p><?php echo html($action->description()) ?></p>
-          </div>    
+          </div>
         <div class="center-text">
         <button class="button" onclick="window.location.href='action'"><h6>Learn more</h6></button>
         </div>
@@ -27,6 +43,10 @@
       <?php endforeach ?>
     </div>
   </div>
+
+
+
+
 </main>
 
 <?php snippet('footer') ?>
