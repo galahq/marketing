c544e9827123d774a60c9b5181651d93ab95bda0
Pearl Zeng <zhuzeng@umich.edu>
2016-08-12 09:19:18 -0400

    Sets up responsive mobile menu.

---
+++ site/snippets/menu-mobile.php
@@ -0,0 +1,34 @@
+<?php
+  // nested menu
+  $items = $pages->visible();
+  // only show the menu if items are available
+  if($items->count()):
+  ?>
+
+  <nav class="site menu-mobile cf">
+    <ul class="menu_drop">
+      <?php foreach($items as $item): ?>
+      <li>
+        <a<?php e($item->isOpen(), ' class="active"') ?> href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a>
+
+        <?php
+
+        // get all children for the current menu item
+        $children = $item->children()->visible();
+
+        // display the submenu if children are available
+        if($children->count() > 0):
+
+        ?>
+        <ul class="second-level">
+          <?php foreach($children as $child): ?>
+          <li><a<?php e($child->isOpen(), ' class="active"') ?> href="<?php echo $child->url() ?>"><?php echo $child->title()->html() ?></a></li>
+          <?php endforeach ?>
+        </ul>
+        <?php endif ?>
+
+      </li>
+      <?php endforeach ?>
+    </ul>
+  </nav>
+<?php endif ?>
--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,6 +1,6 @@
 <nav role="navigation">
 
-  <ul class="menu cf">
+  <ul class="menu cf hide">
     <?php foreach($pages->visible() as $p): ?>
     <li>
       <a class="uppercase<?php e($p->isOpen(), ' active') ?>" href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
