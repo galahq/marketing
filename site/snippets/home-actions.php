b0e3ab7998225dde12cf25c55f74ecf174b739cd
Pearl Zeng <zhuzeng@umich.edu>
2016-08-12 09:17:55 -0400

    Changes alt text.

--- site/snippets/home-actions.php
+++ site/snippets/home-actions.php
@@ -1,6 +1,6 @@
 <div>
   <?php if($image = $action->image()): ?>
-    <img src="<?php echo $image->url() ?>" alt="Icon for <?php echo html($action->title()) ?>">
+    <img src="<?php echo $image->url() ?>" alt="Image for <?php echo html($action->title()) ?>">
   <?php endif ?>
   <div class="actiontext">
   <h5 class="uppercase"><?php echo html($action->title()) ?></h5>
