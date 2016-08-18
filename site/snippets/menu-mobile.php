269aa2d6527538b5c1a1b28f6da44830efeffae3
Pearl Zeng <zhuzeng@umich.edu>
2016-08-18 09:23:25 -0400

    DRY mobile menu call.

--- site/snippets/menu-mobile.php
+++ site/snippets/menu-mobile.php
@@ -1,37 +1,19 @@
-<?php
-// nested menu
-$items = $pages->visible();
-// only show the menu if items are available
-if($items->count()):
-?>
+<?php $items = $pages->visible();?>
 
 <nav class="site menu-mobile">
   <a href="#menu" id="toggle"><span></span></a>
   <div id="menu">
-  <ul class="first-level">
-    <?php foreach($items as $item): ?>
-    <li>
-      <a<?php e($item->isOpen(), ' class="active"') ?> href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a>
-
-      <?php
-
-      // get all children for the current menu item
-      $children = $item->children()->visible();
-
-      // display the submenu if children are available
-      if($children->count() > 0):
-
-      ?>
-      <ul class="second-level">
-        <?php foreach($children as $child): ?>
-        <li><a<?php e($child->isOpen(), ' class="active"') ?> href="<?php echo $child->url() ?>"><?php echo $child->title()->html() ?></a></li>
-        <?php endforeach ?>
-      </ul>
-      <?php endif ?>
-
-    </li>
-    <?php endforeach ?>
-  </ul>
-</div>
+    <ul class="first-level">
+      <?php foreach($items as $item): ?>
+      <li>
+        <a<?php e($item->isOpen(), 'class="active"')?> href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a>
+          <?php foreach($item->children() as $child): ?>
+          <ul class="second-level">
+          <li><a href="<?php echo $child->url() ?>"><?php echo $child->title()?></a></li>
+          </ul>
+          <?php endforeach ?>
+      </li>
+      <?php endforeach ?>
+    </ul>
+  </div>
 </nav>
-<?php endif ?>
