fae611151e412c36ea90f56dc10b0338ec8009cf
Cameron Bothner <cameronbothner@gmail.com>
2016-08-31 13:21:56 -0400

    Makes the mobile menu work: animated in and out, ScrollLock, esc, etc.

--- site/snippets/menu-mobile.php
+++ site/snippets/menu-mobile.php
@@ -1,18 +1,20 @@
 <?php $items = $pages->visible();?>
-<h5 class="button" id="responsive-nav">MENU</h5>
-<nav class="menu-mobile">
-  <div id="menu">
-    <ul class="first-level" style="display: none;">
-      <?php foreach($items as $item): ?>
-      <li>
-        <a href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a>
-          <?php foreach($item->children() as $child): ?>
-          <ul class="second-level">
-          <li><a href="<?php echo $child->url() ?>"><?php echo $child->title()?></a></li>
-          </ul>
-          <?php endforeach ?>
-      </li>
-      <?php endforeach ?>
-    </ul>
-  </div>
+<h5 class="menu-mobile-open button" id="menu-mobile-open">MENU</h5>
+
+<nav id="menu-mobile" class="menu-mobile no-scroll-lock">
+  <button class="menu-mobile-close button">&larr; Close Menu</button>
+  <ul class="first-level">
+    <?php foreach($items as $item): ?>
+    <li>
+      <a href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a>
+        <?php foreach($item->children() as $child): ?>
+        <ul class="second-level">
+        <li><a href="<?php echo $child->url() ?>"><?php echo $child->title()?></a></li>
+        </ul>
+        <?php endforeach ?>
+    </li>
+    <?php endforeach ?>
+  </ul>
 </nav>
+
+<div class="content-mask"></div>
