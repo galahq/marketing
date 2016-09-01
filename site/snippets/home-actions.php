6b1a6c107aca2b5974306abedc90473c89a05118
Pearl Zeng <zhuzeng@umich.edu>
2016-09-01 15:27:01 -0400

    Adds links and interaction to actions and elements section on homepage.

--- site/snippets/home-actions.php
+++ site/snippets/home-actions.php
@@ -1,8 +1,9 @@
-<div>
+<div><a href="<?php echo $action->link() ?>">
   <?php $homethumbImage = $action->files()->filterBy('filename', '*=', '-homethumb')->first()->uri(); ?>
     <img src="<?php echo $homethumbImage ?>" alt="Image for <?php echo html($action->title()) ?>">
   <div class="actiontext">
   <h5 class="uppercase"><?php echo html($action->title()) ?></h5>
   <p><?php echo html($action->tagline()) ?></p>
   </div>
+</a>
 </div>
