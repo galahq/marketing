9d44796b2def659d7b5efe221e30cacd9a564a41
Cameron Bothner <cameronbothner@gmail.com>
2016-09-08 13:01:59 -0400

    Fixes kirby tag for imgix; kirbytext uls.

--- site/tags/imgix.php
+++ site/tags/imgix.php
@@ -4,13 +4,15 @@
 kirbytext::$tags['imgix'] = [
   'attr' => ['alt', 'class', 'size'],
   'html' => function($tag) {
-    return imgix_tag(
+    return '<figure class="'. $tag->attr('class') .'">' .
+    imgix_tag(
       $tag->attr('imgix'),
       $tag->attr('alt'),
       $tag->attr('size', '840'),
       [],
-      ["class" => $tag->attr('class', 'imgix')]
-    );
+      ["class" => $tag->attr('class')]
+    ) .
+    '</figure>';
   }
 ]
 
