93d6c484f0bf85e39562b684f1f44b4881de1488
Pearl Zeng <zhuzeng@umich.edu>
2016-09-15 11:55:10 -0400

    Adds video to vision page.

--- site/accounts/mscpearl.php
+++ site/accounts/mscpearl.php
@@ -7,6 +7,7 @@ password: >
 language: en
 role: admin
 history:
+  - msc/vision
   - msc
   - msc/team
   - element/podcast
--- site/tags/imgix.php
+++ site/tags/imgix.php
@@ -2,30 +2,28 @@
 
 
 kirbytext::$tags['imgix'] = [
-  'attr' => ['alt', 'class', 'size'],
+  'attr' => ['alt', 'class', 'size', 'caption'],
   'html' => function($tag) {
     return '<figure class="'. $tag->attr('class') .'">' .
     imgix_tag(
       $tag->attr('imgix'),
       $tag->attr('alt'),
-      $tag->attr('size', '840'),
-      [],
-      ["class" => $tag->attr('class')]
+      $tag->attr('size', '840')
     ) .
+    '<figcaption>' . $tag->attr('caption') . '</figcaption>' .
     '</figure>';
   }
 ];
 
 kirbytext::$tags['imgix_screenshot'] = [
-  'attr' => ['alt', 'class'],
+  'attr' => ['alt', 'class','caption'],
   'html' => function($tag) {
     return '<figure class="'. $tag->attr('class') .'">' .
     imgix_screenshot_tag(
       $tag->attr('imgix_screenshot'),
-      $tag->attr('alt'),
-      [],
-      ["class" => $tag->attr('class')]
+      $tag->attr('alt')
     ) .
+    '<figcaption class="imgix-screenshot-caption">' . $tag->attr('caption') . '</figcaption>' .
     '</figure>';
   }
 ]
