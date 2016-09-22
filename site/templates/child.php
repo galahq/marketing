1e93189d49294048d3216886e29baf8e13068118
Pearl Zeng <zhuzeng@umich.edu>
2016-09-22 17:06:53 -0400

    Change caption background to ltoffwhite.

--- site/templates/child.php
+++ site/templates/child.php
@@ -1,24 +1,23 @@
 <?php snippet('head') ?>
 <?php snippet('header') ?>
 
-<main class="main child-page" role="main">
+<main class="main" role="main">
+  <section class="site blog cf">
+    <?php echo str_replace('(\\', '(', kirbytext($page->text())) ?>
+  </section>
 
-
-    <section class="site blog cf">
-
-      <?php echo str_replace('(\\', '(', kirbytext($page->text())) ?>
-
-    </section>
-
-    <section class="child-background">
-      <div class="site blog">
-        <?php
-        if($page->content()->has('wbg')) {
-          echo $page->wbg()->kirbytext();
-        }
-        ?>
-      </div>
-
+  <section class="child-background">
+    <div class="site blog">
+      <?php
+      if($page->content()->has('wbg')) {
+        echo $page->wbg()->kirbytext();
+      }
+      ?>
+    </div>
+  </section>
 </main>
 
 <?php snippet('footer') ?>
+
+
+
