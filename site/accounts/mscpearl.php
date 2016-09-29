560efa5434cbe599af98f9ff77abce54799fc76a
Pearl Zeng <zhuzeng@umich.edu>
2016-09-29 09:41:45 -0400

    Changes text in milestone page.

--- site/accounts/mscpearl.php
+++ site/accounts/mscpearl.php
@@ -7,8 +7,8 @@ password: >
 language: en
 role: admin
 history:
+  - msc/milestones
   - msc/team
   - action/teach/evaluation/initial-assessment
   - action/make/proposal
-  - msc/milestones
   - action
