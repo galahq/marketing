8c17841b42dbe14c693f77aa11e9c6245bdb25b7
Pearl Zeng <zhuzeng@umich.edu>
2016-10-18 16:01:20 -0400

    Adds guide template.

--- site/templates/guide.php
+++ site/templates/guide.php
@@ -3,15 +3,33 @@
 
 <main class="main" role="main">
 
-  <div>
+  <nav class="guide-nav">
+    <?php $guideitems = $pages->find('action')->find('make')->find('production-guide')->children() ;?>
+    <ul>
+      <?php foreach($guideitems as $guideitem) :?>
+      <li>
+        <a href="<?php echo $guideitem->url() ?>" class="<?php e($guideitem->isOpen(), 'active') ?>"><?php echo $guideitem->title()?></a>
+      </li>
+      <?php endforeach ?>
+    </ul>
+  </nav>
 
-    <article class="site cf">
+  <article class="guide-content site">
+    <h1><?php echo html($page->title()) ?></h1>
 
-      <?php echo str_replace('(\\', '(', kirbytext($page->text())) ?>
+    <?php if($page->text() == ''):?>
+    <p>Coming soon!</p>
+    <p>We are working hard on improving this section. Thanks for your patience! In case you've got an urgent question, please <a href="../../../contact">contact us via this page</a>.</p>
 
-    </article>
+    <?php else: ?>
 
-  </div>
+    <?php if($page->text()->isNotEmpty()): ?>
+    <p><?php echo $page->text()?></p>
+    <?php endif ?>
+
+    <?php endif ?>
+
+  </article>
 
 </main>
 
