e265c82ede9c766f7024bc1c265e6522197b7231
Pearl Zeng <zhuzeng@umich.edu>
2016-10-20 11:20:22 -0400

    Changes footer link to production guide.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -26,7 +26,7 @@
         <nav>
           <ul class="footermenu">
             <li><a href="<?php echo $pages->find('action')->find('teach')->find('evaluation')->url() ?>"><?php echo $pages->find('action')->find('teach')->find('evaluation')->title() ?></a></li>
-            <li><a href="<?php echo $pages->find('action')->find('make')->url() ?>"><?php echo $pages->find('action')->find('make')->find('production-guide')->title() ?></a></li>
+            <li><a href="<?php echo $pages->find('action')->find('make')->find('production-guide')->url() ?>">Production Guide</a></li>
             <li><a href="<?php echo $pages->find('action')->find('make')->find('proposal')->url() ?>"><?php echo $pages->find('action')->find('make')->find('proposal')->title() ?></a></li>
             <li><a href="<?php echo $pages->find('contact')->url() ?>">Contact Us</a></li>
           </ul>
