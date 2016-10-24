a4da7ba28245e823799b7fbef9e110187a737bd4
Pearl Zeng <zhuzeng@umich.edu>
2016-10-24 11:47:22 -0400

    Updated footer css and template with new class names.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,19 +1,18 @@
-<footer class="footer cf" role="contentinfo">
+<footer class="footer u-clearfix" role="contentinfo">
   <?php
-  // nested menu
-  $items = $pages->visible();
+  $items = $pages->visible();// nested menu
   ?>
-  <div class="site">
-    <div class="footerheading">
+  <div class="t-site">
+    <div class="footer__heading">
       <h4>Start learning with MSC. </h4>
-      <button class="button buttonondark" onclick="window.location.href='http://www.learnmsc.org'">Case Gallery</button>
+      <button class="c-button c-button-t-dark" onclick="window.location.href='http://www.learnmsc.org'">Case Gallery</button>
     </div> <!--footerheading-->
-    <nav class="footerflex flex-container">
+    <nav class="footer__flexcontainer o-flex-container">
       <?php foreach($items as $item): ?>
 
-      <div class="footerflexitem hide">
+      <div class="footer__flexitem u-hide">
         <h4><?php echo $item->title()->html() ?></h4>
-          <ul class="footermenu">
+          <ul class="footer__menu">
             <?php foreach($item->children() as $child): ?>
               <li><a href="<?php echo $child->url() ?>"><?php echo $child->title()?></a></li>
             <?php endforeach ?>
@@ -21,10 +20,10 @@
         </div><!--footerflexitem-->
       <?php endforeach ?>
 
-      <div class="footerflexitem hide">
+      <div class="footer__flexitem u-hide">
         <h4>Resources</h4>
         <nav>
-          <ul class="footermenu">
+          <ul class="footer__menu">
             <li><a href="<?php echo $pages->find('action')->find('teach')->find('evaluation')->url() ?>"><?php echo $pages->find('action')->find('teach')->find('evaluation')->title() ?></a></li>
             <li><a href="<?php echo $pages->find('action')->find('make')->find('production-guide')->url() ?>">Production Guide</a></li>
             <li><a href="<?php echo $pages->find('action')->find('make')->find('proposal')->url() ?>"><?php echo $pages->find('action')->find('make')->find('proposal')->title() ?></a></li>
@@ -34,8 +33,8 @@
       </div><!--footerflexitem-->
     </nav><!--footerflex-->
 
-    <div class="flex-container footerbottom">
-      <div class="flex-growing copyright">
+    <div class="o-flex-container footer__bottom">
+      <div class="o-flex-item-growing footer__copyright">
         <?php echo $site->copyright()->kirbytext() ?>
       </div><!--copyright-->
     </div><!--footerbottom-->
