a0f727d22cfd91bb47a1352c8ccdd0906b706a4c
Pearl Zeng <zhuzeng@umich.edu>
2016-10-25 13:27:10 -0400

    Updates all styles to follow BEM and namespace convention.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,16 +2,16 @@
   <?php snippet('menu-mobile') ?>
   <?php if($page->isHomePage()): ?>
     <?php $bannerImage = imgix_url('topbanner.jpg', ["h" => '900']);?>
-    <section class="intro" style='background: linear-gradient(to bottom, rgba(0,0,0,0) 40%, rgba(53,83,111,0.8) 100%), url("<?php echo $bannerImage ?>") 25%; background-size: cover;' >
-      <div class="header cf headerbg">
-        <div class="site homebanner cf">
-        <a class="logo" href="<?php echo url() ?>">
+    <section class="home__hero" style='background: linear-gradient(to bottom, rgba(0,0,0,0) 40%, rgba(53,83,111,0.8) 100%), url("<?php echo $bannerImage ?>") 25%; background-size: cover;' >
+      <div class="c-header u-clearfix">
+        <div class="t-site homebanner cf">
+        <a class="c-header__logo" href="<?php echo url() ?>">
           <?php imgix('logo.png', $site->title(), 373.33) ?>
         </a>
         <?php snippet('menu') ?>
       </div>
       </div>
-      <div class="site">
+      <div class="t-site">
         <h1><?php echo $page->tagline() ?></h1>
         <?php echo str_replace('(\\', '(', kirbytext($page->subtagline())) ?>
       </div>
@@ -19,21 +19,21 @@
 
  <?php else: ?>
 
-   <section class="header headerbg cf site">
-     <a class="logo" href="<?php echo url() ?>">
+   <section class="c-header u-clearfix t-site">
+     <a class="c-header__logo" href="<?php echo url() ?>">
        <?php imgix('logo.png', $site->title(), 373.33) ?>
      </a>
      <?php snippet('menu') ?>
    </section>
-   <div class="submenu-bg">
-     <div class="site">
+   <div class="c-submenu__container">
+     <div class="t-site">
        <?php snippet('submenu') ?>
      </div>
    </div>
 
    <?php foreach($pages->visible() as $child): ?>
      <?php if($page->isChildOf($child)) { ?>
-     <ul class="hiddencrumb breadcrumb blog site">
+     <ul class="c-breadcrumb--hidden c-breadcrumb t-blog t-site">
        <?php foreach($site->breadcrumb() as $crumb): ?>
        <li>
          <a href="<?php echo $crumb->url() ?>">
@@ -47,7 +47,7 @@
 
    <?php foreach($pages->visible()->children() as $grandchild): ?>
    <?php if($page->isChildOf($grandchild)) { ?>
-     <ul class="breadcrumb partialcrumb blog site">
+     <ul class="c-breadcrumb c-breadcrumb--partial t-blog t-site">
        <li>
          <a href="<?php echo $page->parent()->url() ?>">
            <?php echo html($page->parent()->title()) ?>
@@ -62,7 +62,7 @@
    <?php endforeach ?>
 
    <?php if($page->isInvisible()) { ?>
-     <ul class="breadcrumb partialcrumb blog site">
+     <ul class="c-breadcrumb c-breadcrumb--partial t-blog t-site">
        <li>
          <a href="<?php echo $page->parent()->url() ?>">
            <?php echo html($page->parent()->title()) ?>
@@ -80,11 +80,11 @@
 
       <section class="hero" style='background: linear-gradient(to bottom, rgba(0,0,0,0) 40%, rgba(53,83,111,0.8) 100%), url("<?php echo $bannerImage ?>") 25%; background-size: cover;' >
     <?php else: ?>
-      <section class="hero-child">
+      <section class="child__hero">
     <?php endif ?>
 
-  <div class="site">
-    <div class="centerbox">
+  <div class="t-site">
+    <div class="page__title">
       <h1><?php echo $page->pagetagline() ?></h1>
       <?php echo str_replace('(\\', '(', kirbytext($page->pagesubtagline())) ?>
     </div>
--- site/snippets/menu-mobile.php
+++ site/snippets/menu-mobile.php
@@ -4,13 +4,13 @@
 
 <nav class="c-menu-mobile no-scroll-lock">
   <h5 class="c-menu-mobile--close">&larr; Close Menu</h5>
-  <ul class="c-nest-menu--firstlevel">
+  <ul class="c-nest-menu__firstlevel">
     <li><a href="<?php echo url() ?>">Home</a></li>
     <?php foreach($items as $item): ?>
     <?php $children = $item->children();?>
     <li>
       <a href="<?php echo $item->url() ?>" class="<?php e($item->isOpen() && !$children->findOpen(), 'active') ?>"><?php echo $item->title()->html() ?></a>
-        <ul class="c-nest-menu--secondlevel">
+        <ul class="c-nest-menu__secondlevel">
           <?php foreach($children as $child): ?>
             <li><a href="<?php echo $child->url() ?>" class="<?php e($child->isOpen(), 'active') ?>"><?php echo $child->title()?></a></li>
           <?php endforeach ?>
@@ -18,7 +18,7 @@
     </li>
     <?php endforeach ?>
     <li><a href"0#">Resources</a>
-      <ul class="c-nest-menu--secondlevel">
+      <ul class="c-nest-menu__secondlevel">
         <li><a href="<?php echo $pages->find('action')->find('teach')->find('evaluation')->url() ?>"><?php echo $pages->find('action')->find('teach')->find('evaluation')->title() ?></a></li>
         <li><a href="<?php echo $pages->find('action')->find('make')->find('production-guide')->url() ?>">Production Guide</a></li>
         <li><a href="<?php echo $pages->find('action')->find('make')->find('proposal')->url() ?>"><?php echo $pages->find('action')->find('make')->find('proposal')->title() ?></a></li>
