d0db7f15808c044413bf4ebd8f77bbf213da03a6
Pearl Zeng <zhuzeng@umich.edu>
2016-10-29 14:31:22 -0400

    Adds podcast production guide.

--- site/templates/printproductionguide.php
+++ site/templates/printproductionguide.php
@@ -13,7 +13,7 @@
     <nav class="o-guide__nav">
       <?php $guideitems = $pages->find('action')->find('make')->find('production-guide')->children()->visible() ;?>
       <ul class="c-nest-menu__firstlevel">
-        <li><?php echo $pages->find('action')->find('make')->find('production-guide')->title() ?></li>
+        <li><a href="#<?php echo $pages->find('action')->find('make')->find('production-guide')->title() ?>"><?php echo $pages->find('action')->find('make')->find('production-guide')->title() ?></a></li>
         <?php foreach($guideitems as $guideitem) :?>
         <?php $children = $guideitem->children();?>
         <li>
@@ -32,12 +32,18 @@
       </ul>
     </nav>
 
+    <div>
+        <h1 id="<?php echo $pages->find('action')->find('make')->find('production-guide')->title() ?>"><?php echo $pages->find('action')->find('make')->find('production-guide')->title() ?></h1>
+        <p><?php echo str_replace('(\\', '(', kirbytext($pages->find('action')->find('make')->find('production-guide')->text())) ?></p>
+        <p style="page-break-after:always;"></p>
+    </div>
+
     <?php $guideitems = $pages->find('action')->find('make')->find('production-guide')->children()->visible() ;?>
     <?php foreach($guideitems as $guideitem) :?>
     <?php $children = $guideitem->children();?>
     <?php foreach($children as $child): ?>
     <div>
-        <h1 id="<?php echo html($child->title()) ?>"><?php echo html($child->title()) ?></h1>
+        <h1 id="<?php echo html($child->title()) ?>"><?php echo html($guideitem->title()) ?>: <?php echo html($child->title()) ?></h1>
         <p><?php echo str_replace('(\\', '(', kirbytext($child->text())) ?></p>
         <p style="page-break-after:always;"></p>
     </div>
