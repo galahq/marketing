cdf4b5c6e6e97ef0e06f84c73d6e72641f45d11f
Pearl Zeng <zhuzeng@umich.edu>
2016-10-31 17:17:38 -0400

    Fixes active state on mobile menu.

--- site/snippets/menu-mobile.php
+++ site/snippets/menu-mobile.php
@@ -5,24 +5,55 @@
 <nav class="c-menu-mobile no-scroll-lock">
   <h5 class="c-menu-mobile--close">&larr; Close Menu</h5>
   <ul class="c-nest-menu__firstlevel">
+
     <li><a href="<?php echo url() ?>">Home</a></li>
+
+
     <?php foreach($items as $item): ?>
-    <?php $children = $item->children();?>
-    <li>
-      <a href="<?php echo $item->url() ?>" class="<?php e($item->isOpen() && !$children->findOpen(), 'active') ?>"><?php echo $item->title()->html() ?></a>
-        <ul class="c-nest-menu__secondlevel">
-          <?php foreach($children as $child): ?>
-            <li><a href="<?php echo $child->url() ?>" class="<?php e($child->isOpen(), 'active') ?>"><?php echo $child->title()?></a></li>
-          <?php endforeach ?>
-        </ul>
-    </li>
+      <?php $children = $item->children();?>
+      <li>
+        <a href="<?php echo $item->url() ?>" class="<?php e($item->isOpen() && !$children->findOpen(), 'active') ?>"><?php echo $item->title()->html() ?></a>
+          <ul class="c-nest-menu__secondlevel">
+
+            <?php foreach($children as $child): ?>
+            <?php $grandchildren = $child->children();?>
+
+              <li>
+                <a href="<?php echo $child->url() ?>"
+                   class="<?php e($child->isOpen() && !$grandchildren->findOpen(), 'active') ?>">
+                    <?php echo $child->title()?>
+                </a>
+              </li>
+
+            <?php endforeach ?>
+
+          </ul>
+      </li>
     <?php endforeach ?>
-    <li><a href"0#">Resources</a>
+
+
+
+    <li>
+      <a>Resources</a>
+
       <ul class="c-nest-menu__secondlevel">
-        <li><a href="<?php echo $pages->find('action')->find('teach')->find('evaluation')->url() ?>"><?php echo $pages->find('action')->find('teach')->find('evaluation')->title() ?></a></li>
-        <li><a href="<?php echo $pages->find('action')->find('make')->find('production-guide')->url() ?>">Production Guide</a></li>
-        <li><a href="<?php echo $pages->find('action')->find('make')->find('proposal')->url() ?>"><?php echo $pages->find('action')->find('make')->find('proposal')->title() ?></a></li>
-        <li><a href="<?php echo $pages->find('contact')->url() ?>">Contact Us</a></li>
+
+        <?php $resources = [
+          $pages->find('action')->find('teach')->find('evaluation'),
+          $pages->find('action')->find('make')->find('production-guide'),
+          $pages->find('action')->find('make')->find('proposal'),
+          $pages->find('contact')
+        ];
+
+        foreach($resources as $resource): ?>
+          <li>
+            <a href="<?php echo $resource->url() ?>"
+               class="<?php e($resource->isOpen(), 'active') ?>" >
+                <?php echo $resource->title() ?>
+            </a>
+          </li>
+        <?php endforeach ?>
+
       </ul>
     </li>
   </ul>
