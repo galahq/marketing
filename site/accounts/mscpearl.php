854e284ebf2cea1c6c5e4a09bc2ee42f7c820ddb
Pearl Zeng <zhuzeng@umich.edu>
2016-11-01 12:26:04 -0400

    Adds team formation table to actions/make.

--- site/accounts/mscpearl.php
+++ site/accounts/mscpearl.php
@@ -7,9 +7,8 @@ password: >
 language: en
 role: admin
 history:
-  - >
-    action/make/production-guide/deliverables/teaching-guide
+  - action/make
+  - action/make/production-guide/deliverables/teaching-guide
   - action/make/production-guide/deliverables/podcast
   - action/make/production-guide/deliverables/edgenotes
   - action/make/production-guide/deliverables/narrative
-  - action/make/production-guide/deliverables/cover-page
