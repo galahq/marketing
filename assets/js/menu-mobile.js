74ac5e868016ff01ccdf42f6e7fa6b305be8d903
Pearl Zeng <zhuzeng@umich.edu>
2016-11-02 14:10:54 -0400

    Revise mobile menu js, still not working.

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -44,8 +44,10 @@ $(document).ready(function(){
     if (e.which === 27) { closeMenu(); }
   });
 
-  /*swipe left to close
-  $("nav").on("swipeleft",function(){
-    closeMenu();
-  });*/
+  //swipe left to close
+  $(".c-menu-mobile").on("swipeleft",function(){
+    $("nav.c-menu-mobile").animate({ left: "-100%"}, 200, "easeInCubic", function() { $(this).hide() });
+    $(".u-content-mask").fadeOut();
+    $("body").off("wheel touchmove");
+  });
 });
