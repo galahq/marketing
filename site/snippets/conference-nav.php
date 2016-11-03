29425fca826aecf6c8b29d4e0a0ac574a18e4998
Pearl Zeng <zhuzeng@umich.edu>
2016-11-03 11:31:17 -0400

    Adds active state to global case competition menu.

--- site/snippets/conference-nav.php
+++ site/snippets/conference-nav.php
@@ -1,11 +1,11 @@
 <div class="c-conference__menu">
 <nav class="t-site o-flex-container">
+  <h5>Global Case Competition</h5>
   <?php if($page->hasChildren()) : ?>
     <?php $sections = $page->children()->visible() ;?>
     <?php else: ?>
     <?php $sections = $page->parent()->children()->visible() ;?>
   <?php endif ?>
-  <h5>Global Case Competition</h5>
   <ul class="o-flex-container">
     <?php foreach($sections as $section) :?>
     <li>
