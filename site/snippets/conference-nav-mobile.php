5c9af5f1fbca4849d526045d0a1390a72d7d8944
Pearl Zeng <zhuzeng@umich.edu>
2016-11-08 11:36:28 -0500

    Makes the overview page its separate item on the mobile nav.

--- site/snippets/conference-nav-mobile.php
+++ site/snippets/conference-nav-mobile.php
@@ -1,12 +1,13 @@
 <div class="c-conference__menu--mobile">
 <nav class="t-site o-dropdown">
-  <h5><a href="<?php echo $pages->find('community')->find('case-competition')->url() ?>">Global Case Competition</a><a onclick="dropdown()" class="u-dropdown">▾</a></h5>
+  <h5><a onclick="dropdown()" class="u-dropdown">Global Case Competition ▾</a></h5>
   <?php if($page->hasChildren()) : ?>
     <?php $sections = $page->children()->visible() ;?>
     <?php else: ?>
     <?php $sections = $page->parent()->children()->visible() ;?>
   <?php endif ?>
   <ul id="dropdownNav" class="o-dropdown__nav">
+    <li><a href="<?php echo $pages->find('community')->find('case-competition')->url() ?>" class="<?php e($pages->find('community')->find('case-competition')->isOpen() && !$pages->find('community')->find('case-competition')->children()->findOpen(), 'active') ?>">Overview</a></li>
     <?php foreach($sections as $section) :?>
     <li>
       <a href="<?php echo $section->url() ?>" class="<?php e($section->isOpen(), 'active') ?>"><?php echo $section->title()?></a>
