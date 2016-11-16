9ba2872914f865ae61f20f8844936f610f649126
Pearl Zeng <zhuzeng@umich.edu>
2016-11-16 17:15:14 -0500

    Change font async setting.

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -25,7 +25,7 @@
       href="https://msc-public.imgix.net/msc-favicon.png" />
 
   <script src="https://use.typekit.net/fxq4rje.js"></script>
-  <script>try{Typekit.load({ async: true  });}catch(e){}</script>
+  <script>try{Typekit.load({ async: false  });}catch(e){}</script>
 
 </head>
 <body class="<?php echo str_replace('.', '-', $page->template()) ?>" id="<?php echo $page->uid() ?>">
