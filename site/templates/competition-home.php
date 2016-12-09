ed9ae56968096b71705e85a1b6075fdf2f65a47a
Pearl Zeng <zhuzeng@umich.edu>
2016-12-09 13:25:38 -0500

    Applies some layout of production guide to global case competition page.

--- site/templates/competition-home.php
+++
@@ -1,8 +0,0 @@
-<?php snippet('head') ?>
-<?php snippet('header') ?>
-
-<?php foreach($pages->find('community')->find('case-competition')->visible() as $section): ?>
-  <?php snippet($section->uid()) ?>
-<?php endforeach ?>
-
-<?php snippet('footer') ?>
--- site/templates/competition.php
+++ site/templates/competition.php
@@ -2,10 +2,39 @@
 <?php snippet('header') ?>
 
 <main class="main" role="main">
-  <article class="t-site t-blog">
-    <h1><?php echo html($page->title()) ?></h1>
-    <p><?php echo str_replace('(\\', '(', kirbytext($page->text())) ?></p>
-  </article>
+
+  <div class="o-flex-container t-site">
+
+    <nav class="o-guide__nav o-flex-item-growing">
+      <?php $competitionitems = $pages->find('community')->find('case-competition')->children()->visible() ;?>
+      <ul class="c-nest-menu__firstlevel">
+        <li><a href="<?php echo $pages->find('community')->find('case-competition')->url() ?>"<?php echo $pages->find('community')->find('case-competition')->title() ?></a></li>
+        <?php foreach($competitionitems as $competitionitem) :?>
+        <li>
+          <a href="<?php echo $competitionitem->url() ?>"><?php echo $competitionitem->title()?></a>
+        </li>
+        <?php endforeach ?>
+      </ul>
+    </nav>
+
+    <article class="o-guide__content o-flex-item-growing" id="content">
+      <h1><?php echo html($page->title()) ?></h1>
+
+      <?php if($page->text() == ''):?>
+      <p>Coming soon!</p>
+      <p>We are working hard on improving this section. Thanks for your patience! In case you've got an urgent question, please <a href="../../../contact">contact us via this page</a>.</p>
+
+      <?php else: ?>
+
+      <?php if($page->text()->isNotEmpty()): ?>
+      <p><?php echo str_replace('(\\', '(', kirbytext($page->text())) ?></p>
+      <?php endif ?>
+
+      <?php endif ?>
+
+    </article>
+
+  </div>
 
 </main>
 
