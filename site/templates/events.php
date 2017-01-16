0b7db24ab83af2095217c99fdd4f9385845e7d69
Pearl Zeng <zhuzeng@umich.edu>
2017-01-16 11:27:48 -0500

    Starts html for the events page.

---
+++ site/templates/events.php
@@ -0,0 +1,69 @@
+<?php snippet('head') ?>
+<?php snippet('header') ?>
+
+<main class="main" role="main">
+<div class="t-site">
+<ul>
+  <li>Upcoming Events</li>
+  <li>Previous Events</li>
+</ul>
+
+<form method="get" action="http://www.teachmsc.org">
+        <input type="text"  name="q" size="21" maxlength="120"><input type="submit" value="search">
+</form>
+
+<div>
+<h5>Hosted by:</h5>
+<ul>
+  <li>MSC</li>
+  <li>Other institutions</li>
+</ul>
+</div>
+
+<div>
+<h5>Topic:</h5>
+<ul>
+  <li>Case production</li>
+  <li>Case methodology</li>
+  <li>Assessment and evaluation</li>
+  <li>Case teaching</li>
+  <li>Case selection</li>
+</ul>
+</div>
+
+<div>
+<h5>Selected filters:</h5>
+<ul>
+  <li>MSC</li>
+  <li>Case methodology</li>
+</ul>
+<h6>Clear all filters</h6>
+</div>
+
+<div>
+  <h2>Understanding Case-based teaching and learning</h2>
+  <ul>
+    <li>Wednesday, January 11, 2017</li>
+    <li>Lecture</li>
+    <li>Public</li>
+    <li>Hosted by: MSC</li>
+  </ul>
+  <ul>
+    <li>3:00</li>
+    <li>Ann Arbor, MI</li>
+    <li>Learn more/Register</li>
+  </ul>
+</div>
+
+</div><!--t-site-->
+
+
+</body>
+</html>
+
+
+
+
+</main>
+
+<?php snippet('footer') ?>
