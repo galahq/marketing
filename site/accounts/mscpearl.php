55c36051c54ac018541f6e9aa2182924432e01a4
Pearl Zeng <zhuzeng@umich.edu>
2017-02-02 10:37:00 -0500

    Update production guide to include in-kind support, and updates submit proposal page to include link to funding model.

--- site/accounts/mscpearl.php
+++ site/accounts/mscpearl.php
@@ -8,7 +8,7 @@ language: en
 role: admin
 history:
   - action/make/proposal
+  - action/make/production-guide/process/funding-disbursement
   - action/make/production-guide/deliverables/podcast
   - action/make/production-guide/deliverables/teaching-guide
   - action/make/production-guide/deliverables/narrative
-  - action/make/production-guide/deliverables/cover-page
