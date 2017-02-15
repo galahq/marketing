f2a0188a26cccf32badbedc09a703ef9b377553b
Pearl Zeng <zhuzeng@umich.edu>
2017-02-15 10:42:26 -0500

    Switches title order to be page title first and then site title.

--- site/accounts/mscpearl.php
+++ site/accounts/mscpearl.php
@@ -7,8 +7,8 @@ password: >
 language: en
 role: admin
 history:
+  - events/previous-events
   - home
   - events
   - action/make/production-guide/process/funding-disbursement
   - action/make/proposal
-  - action/make/production-guide/deliverables/podcast
