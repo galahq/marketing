28b73bfdbbc54b545c0951b3e4eaba69acf24851
Pearl Zeng <zhuzeng@umich.edu>
2017-04-11 13:27:22 -0400

    Adds date for latest timeline item; refactor css.

--- site/accounts/mscpearl.php
+++ site/accounts/mscpearl.php
@@ -7,8 +7,8 @@ password: >
 language: en
 role: admin
 history:
+  - msc/milestones
   - events
   - action/make/production-guide/deliverables/cover-page
-  - msc/milestones
   - home
   - msc/vision
--- site/snippets/timeline.php
+++ site/snippets/timeline.php
@@ -1,9 +1,10 @@
 <?php if($page->timeline()->isNotEmpty()) { ?>
-  <section id="c-timeline" class="c-timeline__container t-site">
+  <section class="c-timeline__container t-site u-clearfix">
     <?php foreach($page->timeline()->toStructure()->sortBy($sort='date', $direction='desc') as $timelineitem): ?>
       <div class="c-timeline__block">
-        <div class="c-timeline__img">
-        </div> <!-- timeline-img -->
+
+        <div class="c-timeline__dot">
+        </div> <!-- the blue dot -->
 
         <div class="c-timeline__content">
           <?php if($timelineitem->picture()->isNotEmpty()) { ?>
@@ -12,9 +13,10 @@
           <span class="c-timeline__date"><?php echo $timelineitem->dateofevent() ?></span>
           <?php echo str_replace('(\\', '(', kirbytext($timelineitem->content())) ?>
           <?php if($timelineitem->newslink()->isNotEmpty()) { ?>
-            <a href="<?php echo $timelineitem->newslink() ?>" class="c-timeline__readmore">Read more ›</a>
+            <a href="<?php echo $timelineitem->newslink() ?>" target="_blank" class="c-timeline__readmore">Read more ›</a>
           <?php } ?>
         </div> <!-- timeline-content -->
+
       </div> <!-- timeline-block -->
     <?php endforeach ?>
   </section>
