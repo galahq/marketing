97e6ca29ee2e455663fa3eca9feb588ab18eb642
Pearl Zeng <zhuzeng@umich.edu>
2018-02-16 10:52:00 -0500

    Updates content

--- site/accounts/mscpearl.php
+++ site/accounts/mscpearl.php
@@ -7,8 +7,8 @@ password: >
 language: en
 role: admin
 history:
-  - msc/team
-  - action/make/proposal
-  - home
   - events
+  - home
+  - action/make/proposal
+  - msc/team
   - msc/milestones/strategic-advisors-meeting-2017
