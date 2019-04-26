88c1aef4427d0e38a1644483a63c54525e940d85
Cameron Bothner <cameronbothner@gmail.com>
2019-04-26 11:52:04 -0400

    Give blueprints human readable titles

--- site/blueprints/elementparent.php
+++ site/blueprints/elementparent.php
@@ -1,6 +1,6 @@
 <?php if(!defined('KIRBY')) exit ?>
 
-title: Page
+title: Elements Index
 pages: true
 files: true
 fields:
--- site/blueprints/events.php
+++ site/blueprints/events.php
@@ -1,6 +1,6 @@
 <?php if(!defined('KIRBY')) exit ?>
 
-title: Page
+title: Events
 pages: true
 files: true
 fields:
